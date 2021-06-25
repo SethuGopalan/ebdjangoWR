d94d5de26239bb962fd546473983261c6164e15f
Sethu Gopalan <Sethu.Gopalan@cytel.com>
2021-06-25 13:47:48 -0400

    views seting changed nad added more weathwer tabs

--- weather/views.py
+++ weather/views.py
@@ -8,9 +8,9 @@ import requests
 
 def index(request):
 
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=15a620071d9a016f64293138aab1045a'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=8b65e9ccfc145bb26cedb47690ed20e2'
 
-    city = 'Massachusetts'
+    city = 'Woburn'
     
     r = requests.get(url.format(city)).json()
 
@@ -25,8 +25,49 @@ def index(request):
          'icon'       : r['weather'][0]['icon']
     }
 
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=15a620071d9a016f64293138aab1045a'
+
+    city = 'Bedford'
+    
+    r = requests.get(url.format(city)).json()
+
+    # print(r.text)
+
+    city_weather1 = {
+
+        'city' : city,
+
+        'temperature' : r['main']['temp'],
+        'description' : r['weather'][0]['description'],
+         'icon'       : r['weather'][0]['icon']
+    }
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=15a620071d9a016f64293138aab1045a'
+
+    city = 'Massachusetts'
+
+    # for x in city: 
+    #     if x == 'Massachusetts':
+    #         r = requests.get(url.format(city)).json()
+    #         if x == "NewYork":
+    #             r = requests.get(url.format(city)).json()
+    #             if x == "Maine":
+    r = requests.get(url.format(city)).json()
+
+    # print(r.text)
+
+    city_weather2 = {
+
+        'city' : city,
+
+        'temperature' : r['main']['temp'],
+        'description' : r['weather'][0]['description'],
+         'icon'       : r['weather'][0]['icon']
+    }
     print(city_weather)
     context ={
-        'city_weather':city_weather
+        'city_weather':city_weather,
+        'city_weather1':city_weather1,
+        'city_weather2':city_weather2
+
     }
     return render(request, 'index.html',context)
